6f82bd2b455935d5891fe2a0c1355e5bf8079e92
kanipriya5689-png <kanipriya5689@gmail.com>
2026-03-16 22:51:41 +0530

    Update 3.palindrome.js

--- Day - 8 (Recursion-1)/3.palindrome.js
+++ Day - 8 (Recursion-1)/3.palindrome.js
@@ -1,1 +1,19 @@
 // https://leetcode.com/problems/valid-palindrome/
+fun palindrome(s: String, S: Int, E: Int): Boolean {
+
+    if (S >= E) {
+        return true
+    }
+
+    if (s[S] != s[E]) {
+        return false
+    }
+
+    return palindrome(s, S + 1, E - 1)
+}
+
+fun main() {
+    val s = "raceacar"
+    val result = palindrome(s, 0, s.length - 1)
+    println(result)
+}
